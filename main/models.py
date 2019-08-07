89829b5aa907b58dd4d5ce0edf81a388291ec857
netteNz <lugo.emanuel@gmail.com>
2019-08-06 21:33:39 -0400

    Changes to the admin page

--- main/models.py
+++ main/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
 class Tutorial(models.Model):
     tutorial_title = models.CharField(max_length=200)
     tutorial_content = models.TextField()
-    tutorial_published = models.DateTimeField("Date published")
+    tutorial_published = models.DateTimeField("Date published", default=datetime.now())
 
     def __str__(self):
         return self.tutorial_title
